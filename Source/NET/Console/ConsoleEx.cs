a75c9958d12ef36df0cbd1c3613288caaf1de31e
Adam Wilson <adamw@prospectivesoftware.com>
2013-07-11 01:00:32 -0700

    Started working on Extended Console capabilities.

---
+++ Source/NET/Console/ConsoleEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	public class ConsoleEx
+	{
+		public bool IsInteractive { get; private set; }
+
+		public ConsoleEx(bool IsInteractive = false)
+		{
+			this.IsInteractive = IsInteractive;
+		}
+	}
+}
---
+++ Source/NET/Console/ConsoleExModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Console
+{
+	public abstract class ConsoleExModule
+	{
+		public string Verb { get; protected set; }
+
+		protected ConsoleExModule(string Verb)
+		{
+			this.Verb = Verb;
+		}
+	}
+}
