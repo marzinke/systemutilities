d3a3ffec216e7f7fedeb0ee6198418db1fce1160
Adam Wilson <adamw@prospectivesoftware.com>
2013-08-07 16:29:03 -0700

    Added extension method ToByteArray() to BitArray

---
+++ Source/NET/Collections/BitArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections
+{
+	public static class BitArrayExtensions
+	{
+		public static byte[] ToByteArray(this BitArray bits)
+		{
+			var ret = new byte[(bits.Length - 1) / 8 + 1];
+			bits.CopyTo(ret, 0);
+			return ret;
+		}
+	}
+}
